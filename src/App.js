634ca4d28eb6a53489f56ed1ed2960883bee4614
Dawid Neumann <dawid.neumann.official@gmail.com>
2020-11-19 19:44:57 +0100

    routing and cart on button

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
+
+import Menu from './components/menu/Menu.jsx';
+import HomePage from './pages/homePage/HomePage.jsx';
+import ShopPage from './pages/shopPage/ShopPage.jsx';
 
 import './App.css';
-import ProductList from './components/shop/ProductsList/ProductsList.jsx';
 
 function App() {
   return (
     <div className="App">
-      <header>
-        <nav>
-          <h2>Shop</h2>
-        </nav>
-      </header>
-      <section>
-        <ProductList/>
-      </section>
-    </div>
+      <Router>
+        {<Menu/>}
+        <div className="context--container">
+          <Switch>
+            <Route path="/" exact>
+              <HomePage/>
+            </Route>
+            <Route path="/shop">
+              <ShopPage/>
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    </div> 
   );
 }
 
